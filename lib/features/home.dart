12ec8689cd8f6d4bee9813ba995e0e6f76b53b66
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 09:03:14 +0100

    updated home appbar

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mood_tracker/util/widgets/mood_app_text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0, 
+        title: KMoodText('Home', variant: MoodTextVariant.header)),
+    );
   }
 }
