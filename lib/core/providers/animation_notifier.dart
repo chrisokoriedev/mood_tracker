8df03859f9759e7d8b2b8bd5eab1630591576374
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 08:28:23 +0100

    updated the notifier

--- lib/core/providers/animation_notifier.dart
+++ lib/core/providers/animation_notifier.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final animationNotifierProvider = NotifierProvider<AnimationNotifier, String?>(
@@ -5,12 +7,22 @@ final animationNotifierProvider = NotifierProvider<AnimationNotifier, String?>(
 );
 
 class AnimationNotifier extends Notifier<String?> {
+  Timer? _timer;
   @override
   String? build() {
+    ref.onDispose(() => _timer?.cancel());
     return null;
   }
 
   void triggerAnimation(String entryId) {
-   
+    _timer?.cancel();
+    if (state == entryId) {
+      state = null;
+    } else {
+      state = entryId;
+      _timer = Timer(const Duration(milliseconds: 800), () {
+        state = null;
+      });
+    }
   }
 }
