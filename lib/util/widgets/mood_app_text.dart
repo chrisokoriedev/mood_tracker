d57dcf3c272909c727ad29ef90e7037c1a493007
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 08:50:19 +0100

    updated mood text

--- lib/util/widgets/mood_app_text.dart
+++ lib/util/widgets/mood_app_text.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+enum MoodTextVariant { header, normal, small }
+
 class KMoodText extends StatelessWidget {
   const KMoodText(
     this.text, {
     super.key,
+    this.variant = MoodTextVariant.normal,
     this.style,
     this.textAlign,
     this.maxLines,
@@ -11,6 +14,7 @@ class KMoodText extends StatelessWidget {
   });
 
   final String text;
+  final MoodTextVariant variant;
   final TextStyle? style;
   final TextAlign? textAlign;
   final int? maxLines;
@@ -18,9 +22,17 @@ class KMoodText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    final base = switch (variant) {
+      MoodTextVariant.header => textTheme.titleLarge,
+      MoodTextVariant.normal => textTheme.bodyMedium,
+      MoodTextVariant.small => textTheme.bodySmall,
+    };
+
     return Text(
       text,
-      style: style,
+      style: (base ?? const TextStyle()).merge(style),
       textAlign: textAlign,
       maxLines: maxLines,
       overflow: overflow,
