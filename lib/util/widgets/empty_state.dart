b3417b037d00f8a5f1fb9cce1fc45affe76c2bdd
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 08:44:21 +0100

    working on reusabled text

--- lib/util/widgets/empty_state.dart
+++ lib/util/widgets/empty_state.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
 
+class EmptyState extends StatelessWidget {
+  const EmptyState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'No mood entries yet.\nTap a mood above to log one.',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+    );
+  }
+}
