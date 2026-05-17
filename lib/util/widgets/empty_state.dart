465661e6d0d23b4e1f29e5b2ef4c9ca0d80948b9
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 08:37:57 +0100

    updated color code placement

--- lib/util/widgets/empty_state.dart
+++ lib/util/widgets/empty_state.dart
@@ -1,13 +1,2 @@
 import 'package:flutter/material.dart';
 
-final class AppColors {
-  AppColors._();
-
-  static const Color happy = Color(0xFFFFC107);
-  static const Color neutral = Color(0xFF607D8B);
-  static const Color sad = Color(0xFF5C6BC0);
-  static const Color excited = Color(0xFFE64A19);
-  static const Color anxious = Color(0xFF26A69A);
-
-  
-}
