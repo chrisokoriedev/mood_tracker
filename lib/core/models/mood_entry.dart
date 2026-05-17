fa9f199637424250a49b10deb47384f530b00314
chrisokorieedev <okoriec01@gmail.com>
2026-05-17 08:07:12 +0100

    added mood entry hive class

---
+++ lib/core/models/mood_entry.dart
@@ -0,0 +1,15 @@
+import 'package:hive/hive.dart';
+
+part 'mood_entry.g.dart';
+
+@HiveType(typeId: 0)
+class MoodEntry {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String mood;
+  @HiveField(2)
+  final DateTime date;
+
+  MoodEntry({required this.id, required this.mood, required this.date});
+}
